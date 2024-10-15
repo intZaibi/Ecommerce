e5157f962c21d2f2a59ac3191b86d1007bb154c9
intZaibi <shahzaibalisomroo@gmail.com>
2024-10-15 06:58:31 +0500

    minor changes

--- app/api/signUp/route.js
+++ app/api/signUp/route.js
@@ -5,11 +5,9 @@ export async function POST(req) {
   const {email} = await req.json();
   
   try {
-    const data = await db.query(`select * from allproducts`);
-    console.log(`db data: `, data)
-    const res = db.query('INSERT INTO user (email) VALUES (?)', [email])
-    console.log(`response by db: `, res)
-    return NextResponse.json({ message: `Email: ${email} is added!\n db: ${data}` }, {status: 200})
+    await db.query(`select * from allproducts`);
+    db.query('INSERT INTO users (email) VALUES (?)', [email])
+    return NextResponse.json({ message: `Email: ${email} is added!` }, {status: 200})
   } catch (error) {
     return NextResponse.json({ error: error }, {status: 400})
   }

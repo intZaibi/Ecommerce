c7c1b8a6cb3f7d823345702ffff5933a3f26065a
intZaibi <shahzaibalisomroo@gmail.com>
2024-10-15 06:43:15 +0500

    minor changes

--- app/api/signUp/route.js
+++ app/api/signUp/route.js
@@ -9,7 +9,7 @@ export async function POST(req) {
     console.log(data)
     const res = db.query('INSERT INTO user (email) VALUES (?)', [email])
     console.log(res)
-    return NextResponse.json({ message: `Email: ${email} is added!` }, {status: 200})
+    return NextResponse.json({ message: `Email: ${email} is added!\n db: ${data}` }, {status: 200})
   } catch (error) {
     return NextResponse.json({ error: error }, {status: 400})
   }

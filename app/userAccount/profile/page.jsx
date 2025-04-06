080736e7c3893b754c0adc38f710fa062de8c48c
IntZaibi <154490572+intZaibi@users.noreply.github.com>
2025-04-06 11:55:31 +0500

    Update page.jsx

--- app/userAccount/profile/page.jsx
+++ app/userAccount/profile/page.jsx
@@ -153,7 +153,7 @@ export default function Profile() {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
               </svg>
             </div>
-            <div id="dropdown" className="py-10 w-48 items-center cursor-auto z-10 top-28 flex-col gap-8 rounded-[0_16px_16px_16px] bg-white shadow-[0_40px_20px_#080f341a] absolute hidden">
+            <div id="dropdown" className="py-10 w-48 items-center cursor-auto z-10 top-24 flex-col gap-8 rounded-[0_16px_16px_16px] bg-white shadow-[0_40px_20px_#080f341a] absolute hidden">
               <Link href="/userAccount/orders" className="flex w-full justify-center">
                 Orders
               </Link>
